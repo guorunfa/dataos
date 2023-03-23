45bc160672ece0e9e21907fdd76a4590733d964e
guorunfa <guorunfa@aliyun.com>
2023-03-23 18:19:22 +0800

    add-login未完成

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 低代码平台Vue 3 + TypeScript + Vite
+
+#### setup中使用i18n
+``` import i18n from '@/i18n'
+//调用
+console.log('18', i18n.global.t('global.sys_set')) ```
--- src/components/Test1.vue
+++ src/components/Test1.vue
@@ -1,22 +1,28 @@
 <script setup lang="ts">
 import i18n from '@/i18n'
+import { FormInst, useMessage } from 'naive-ui'
 import { ref, getCurrentInstance } from 'vue'
 //调用
 console.log('18', i18n.global.t('global.sys_set'))
-
-// import { I18n } from '@/components/I18n'
-const _thiss: any = getCurrentInstance()?.appContext.config.globalProperties
-console.log('getCurrentInstance()?.appContext.config.globalProperties', getCurrentInstance()?.appContext.config.globalProperties)
-
-// 此处可打印_this查看是否存在$t方法 如果$t方法不存在
-//检查一下i18n配置globalInjection: true是否发开如果打开了仍无效 查看下方代码
+const formRef = ref<FormInst | null>(null)
+const message = useMessage()
 </script>
 
 <template>
-  <div class="card">
+  <div class="home-header"></div>
+  <div class="home-body">
     <p>
-      testtttt
-      <code>components/test.vue {{ $t('global.sys_set') }}</code>
+      <n-form ref="formRef" inline :label-width="80" :model="formValue" :rules="rules" :size="size">
+        <n-form-item label="$t('global.form_password')" path="user.name">
+          <n-input v-model:value="formValue.user.name" placeholder="输入账号" />
+        </n-form-item>
+        <n-form-item label="密码" path="phone">
+          <n-input v-model:value="formValue.phone" placeholder="输入密码" />
+        </n-form-item>
+        <n-form-item>
+          <n-button attr-type="button" @click="handleValidateClick"> 验证 </n-button>
+        </n-form-item>
+      </n-form>
     </p>
   </div>
 </template>
