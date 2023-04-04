6be84a8982837dc75504a3d963aa358162f62675
guorunfa <guorunfa@aliyun.com>
2023-04-04 10:30:43 +0800

    add:lang

--- src/components/LangSelect/index.vue
+++ src/components/LangSelect/index.vue
@@ -1,24 +1,24 @@
 <template>
-  <!-- <n-dropdown trigger="hover" @select="handleSelect" :show-arrow="true" :options="options">
+  <n-dropdown trigger="hover" @select="handleSelect" :show-arrow="true" :options="options">
     <n-button quaternary>
       <n-icon size="20" :depth="1">
         <language-icon></language-icon>
       </n-icon>
     </n-button>
-  </n-dropdown> -->
+  </n-dropdown>
 </template>
 
 <script lang="ts" setup>
 import { useLangStore } from '@/store/modules/langStore/langStore'
-// import { langList } from '@/i18n/index'
-// import { LangEnum } from '@/enums/styleEnum'
+import { langList } from '@/i18n/index'
+import { LangEnum } from '@/enums/themeEnum'
 import { icon } from '@/plugins'
 
 const { LanguageIcon } = icon.ionicons5
 const langStore = useLangStore()
-// const options = langList
+const options = langList
 
-// const handleSelect = (key: LangEnum) => {
-//   langStore.changeLang(key)
-// }
+const handleSelect = (key: LangEnum) => {
+  langStore.changeLang(key)
+}
 </script>
